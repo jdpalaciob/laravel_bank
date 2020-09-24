b1edf912bd71a55519bfb6cbab4fc43407a187a9
jdpalaciob <jdpalaciob@gmail.com>
2020-09-24 16:57:19 -0500

    Primer commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>Laravel Bank</title>
+@endsection
+
+@section('content')
+    @if (Route::has('login'))
+    <div class="top-right links">
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+        @endauth
+    </div>
+    @endif
+
+    <div class="content">
+    <div class="title m-b-md">
+        LARAVEL BANK
+    </div>
+
+    <div class="links">
+        <a href="">Users</a>
+        <a href="">Accounts</a>
+    </div>
+
+@endsection
