504c3847dd739021725cf437c14220cd30483c13
jdpalaciob <jdpalaciob@gmail.com>
2020-09-24 17:25:00 -0500

    Crear modelos y controladores basicos (clients, accounts). Metodo index para ambos.

---
+++ app/Client.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = ['first_name', 'last_name', 'phone_number', 'email', 'username'];
+}
