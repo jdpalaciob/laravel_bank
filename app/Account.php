44bc920f1725e207a1ccc552ccb37cc1f4ed03d6
jdpalaciob <jdpalaciob@gmail.com>
2020-09-25 08:49:49 -0500

    Crear la relacion One to One entre Account y AccountTypes. Es necesario el belongsTo en el modelo principal (tabla que recibe el Foreign Key) y el hasOne del modelo secundario.

--- app/Account.php
+++ app/Account.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     protected $fillable = ['account_number', 'account_type', 'created', 'active', 'user_id'];
+
+    public function accountType() 
+    {
+        return $this->belongsTo('App\AccountType', 'account_type', 'id');
+    }
 }
--- app/AccountType.php
+++ app/AccountType.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class AccountType extends Model
 {
     public $fillable = ['account_type'];
+
+    public function account()
+    {
+        return $this->hasOne('App\Account');
+    }
 }
