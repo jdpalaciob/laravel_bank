3f74788fafa54dda30e5d3220d1292b67f50681c
jdpalaciob <jdpalaciob@gmail.com>
2020-09-25 12:13:07 -0500

    Añadir relaciones OneToMany a los modelos Client y Account.

--- app/Account.php
+++ app/Account.php
@@ -7,12 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['account_number', 'account_type_id', 'created', 'active', 'user_id'];
+    protected $fillable = ['account_number', 'account_type_id', 'created', 'active', 'client_id'];
     //account_id->int
     
 
     public function account_type() 
     {
         return $this->belongsTo('App\AccountType', 'account_type_id', 'id');//->select('id','account_type');
+    }
+
+    public function client()
+    {
+        return $this->belongsTo('App\Client', 'client_id', 'id');
     }
 }
--- app/Client.php
+++ app/Client.php
@@ -8,4 +8,9 @@ class Client extends Model
 {
     public $timestamps = false;
     protected $fillable = ['first_name', 'last_name', 'phone_number', 'email', 'username'];
+
+    public function accounts()
+    {
+        return $this->hasMany('App\Account', 'client_id', 'id');
+    }
 }
