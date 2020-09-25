b21ddfc284b4c3251da27e5a9ff51b8ceda330e9
jdpalaciob <jdpalaciob@gmail.com>
2020-09-25 15:52:41 -0500

    Crear el croquis del view para el create de Client.

---
+++ resources/views/clients/create.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('title')
+    <title>Sign up Client</title>
+@endsection
+
+@section('content')
+
+    <div class="container">
+
+        <div class="links">
+            <a href="">&laquo; Back</a>
+        </div>
+        <div class="title m-d-md">
+            New Client
+        </div>
+
+        <div class="container">
+            <form action="{{route('clients.store')}}">
+                @csrf
+                <dl>
+                    <dt>First Name</dt>
+                    <dd>
+                        <input type="text" name="first_name" id="first_name">
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>Last Name</dt>
+                    <dd>
+                        <input type="text" name="last_name" id="last_name">
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>Phone Number</dt>
+                    <dd>
+                        <input type="text" name="phone_number" id="phone_number">
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>Email</dt>
+                    <dd>
+                        <input type="text" name="email" id="email">
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>Username</dt>
+                    <dd>
+                        <input type="text" name="username" id="username">
+                    </dd>
+                </dl>
+                <button><b>Submit</b></button>
+
+            </form>
+        </div>
+    </div>
+
+    
+@endsection
