1d2761246f205f17b9173512855220fb2ed95350
jdpalaciob <jdpalaciob@gmail.com>
2020-09-28 16:35:28 -0500

    Corregir show en  AccountControler (ausencia de view, mala estructura del arreglo final).

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -69,18 +69,20 @@ class AccountController extends Controller
      */
     public function show(Account $account)
     {
-        $owner = $account->client->first_name . $account->client->last_name;
-        dd($owner);
+        $owner = $account->client->first_name . ' ' . $account->client->last_name;
+        // dd($owner);
 
         $info = [
             'Number' => $account->account_number,
             'Type' => $account->account_type->account_type,
             'Owner' => $owner,
-            'Owner Username' => $account->client->username,
+            'Username' => $account->client->username,
             'Owner Phone' => $account->client->phone_number,
             'Owner Email' => $account->client->email,
-            'Active' => $account->active === 1 ? 'YES' : 'NO'
+            'Active' => $account->active === 1 ? 'YES' : 'NO',
+            'Open Date' => $account->created
         ];
+        return view('accounts.show', ['info' => $info]);
     }
 
     /**
